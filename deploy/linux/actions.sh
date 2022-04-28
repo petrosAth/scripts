dd010ecd900c7dd31c4cdc286926ec3b9bf6b157
petrosAth <develop@athanasoulis.me>
2022-04-29 01:52:17 +0300

    Add okular in actions list

--- deploy/linux/actions.sh
+++ deploy/linux/actions.sh
@@ -58,6 +58,7 @@ actions_list=(
     "install_nerd_fonts_jetbrains_mono"
 #|-< Editing >----------------------------------------------------------------|#
     "install_onlyoffice"
+    "install_okular"
     "install_blender"
     "install_inkscape"
     "install_handbrake"
@@ -401,6 +402,12 @@ declare -A install_onlyoffice=(
     [interface]="gui"
     [message_process]="* Installing OnlyOffice. Open-source office suite that combines text, spreadsheet and presentation editors "
     [arch]="paru -S --needed onlyoffice-bin"
+)
+declare -A install_okular=(
+    [interface]="gui"
+    [message_process]="* Installing Okular document viewer "
+    [arch]="sudo pacman -S --needed okular"
+    [manjaro]="sudo pacman -S --needed okular"
 )
 declare -A install_blender=(
     [interface]="gui"
