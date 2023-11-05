13e2863168ebb3e68e632e238fc7cdfb2f85805d
petrosAth <develop@athanasoulis.me>
2023-11-05 16:22:05 +0200

    init-vm.py - Add remote desktop gui

--- init-vm.py
+++ init-vm.py
@@ -1,12 +1,21 @@
 #! /usr/bin/env python
 
-import subprocess, sys, time
+import os, subprocess, sys, time
 
 def start_vm(vm):
     subprocess.run(["virsh", "--connect", "qemu:///system", "start", vm])
 
 def virt_manager(vm):
     subprocess.run(["virt-manager", "--connect", "qemu:///system", "--show-domain-console", vm])
+
+def remote_desktop(vm):
+    home_path = os.getenv("HOME")
+    remmina_path = ".local/share/remmina"
+    has_rd = { "winWS": "group_rdp_winws_192-168-200-121" }
+
+    if vm in has_rd.keys():
+        time.sleep(10)
+        subprocess.run(["remmina", "-c", f"{home_path}/{remmina_path}/{has_rd[vm]}.remmina"])
 
 def looking_glass(vm):
     has_lg = [ "winWS" ]
@@ -18,6 +27,7 @@ def looking_glass(vm):
 
 def show_vm(vm, gui):
     virt_manager(vm)
+    remote_desktop(vm)
     looking_glass(vm)
 
 def init(vm, gui):
