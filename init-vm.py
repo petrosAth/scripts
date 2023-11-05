b55142801eeac064c369accf8fc23ec638377a29
petrosAth <develop@athanasoulis.me>
2023-11-05 15:45:34 +0200

    init-vm.py - Add show_vm and virt_manager functions

--- init-vm.py
+++ init-vm.py
@@ -5,8 +5,14 @@ import subprocess, sys
 def start_vm(vm):
     subprocess.run(["virsh", "--connect", "qemu:///system", "start", vm])
 
+def virt_manager(vm):
+    subprocess.run(["virt-manager", "--connect", "qemu:///system", "--show-domain-console", vm])
+
+def show_vm(vm, gui):
+    virt_manager(vm)
+
 def init(vm, gui):
     start_vm(vm)
+    show_vm(vm, gui)
 
-args = sys.argv
-init(args[1], args[2])
+init(sys.argv[1], sys.argv[2])
