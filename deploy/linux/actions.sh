203ebc4c9902747669666a9e69a1df4bc905371e
petrosAth <develop@athanasoulis.me>
2023-12-03 21:17:47 +0200

    actions.sh - Add France, Germany and Switzerland in reflector's countries

--- deploy/linux/actions.sh
+++ deploy/linux/actions.sh
@@ -99,7 +99,7 @@ declare -A install_reflector=(
     [message_process]="* Installing Reflector. A Pacman mirror list manager "
     [arch]="4"
     [arch1]="sudo pacman -S --needed reflector"
-    [arch2]="sudo reflector --country Greece --latest 5 --sort rate --age 6 --save /etc/pacman.d/mirrorlist"
+    [arch2]="sudo reflector --country 'France,Germany,Greece,Switzerland' --latest 5 --sort rate --age 6 --save /etc/pacman.d/mirrorlist"
     [arch3]="sudo systemctl enable --now reflector.service"
     [arch4]="sudo systemctl enable --now reflector.timer"
 )
