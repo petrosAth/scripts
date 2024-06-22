62064dcb9be64ef7d757a82958db6743fc43912a
petrosAth <develop@athanasoulis.me>
2024-06-22 12:06:14 +0300

    Add iommu group check script and update launch-vm with new vm names and IPs

--- launch-vm.sh
+++ launch-vm.sh
@@ -12,22 +12,41 @@ virt_manager() {
     virt-manager --connect qemu:///system --show-domain-console ${vm} &
 }
 
+wait_timer() {
+    echo "Please wait while the VM is booting"
+    for ((i = $1; i > 0; i--)); do
+        echo "$i..."
+        sleep 1
+    done
+}
+
 remote_desktop() {
     vm=$1
 
-    if [[ ${vm} = "winWS" ]]; then
-        sleep 10
-        remmina -c /home/petrosath/.local/share/remmina/group_rdp_winws_192-168-200-121.remmina &
+    if [[ ${vm} = "winVMdesign" ]]; then
+        wait_timer 10
+        remmina -c /home/petrosath/.local/share/remmina/group_rdp_winvmdesign_192-168-122-3.remmina &
+        exit
+    elif [[ ${vm} = "winVMmain" ]]; then
+        wait_timer 10
+        remmina -c /home/petrosath/.local/share/remmina/group_rdp_winvmmain_192-168-122.134.remmina &
+        exit
+    elif [[ ${vm} = "winVMwork" ]]; then
+        wait_timer 10
+        remmina -c /home/petrosath/.local/share/remmina/group_rdp_winvmwork_192-168-122.54.remmina &
+        exit
     fi
 }
 
 looking_glass() {
     vm=$1
+    is_lg_vm=("winVMdesign" "winVMmain" "winVMwork")
 
-    if [[ ${vm} = "winWS" ]]; then
-        sleep 2
-        looking-glass-client &
-    fi
+    for vm in ${is_lg_vm}; do
+        wait_timer 2
+        looking-glass-client win:size=1920x1080 &
+        exit
+    done
 }
 
 start_gui() {
@@ -38,15 +57,17 @@ start_gui() {
         remote_desktop ${vm}
     elif [[ ${gui} = "lg" ]]; then
         looking_glass ${vm}
-    else
+    elif [[ ${gui} = "vm" ]]; then
         virt_manager ${vm}
+    else
+        exit
     fi
 }
 
 init() {
     vm=$1
     gui=$2
-    is_valid_vm=("arch" "pop_os" "testDE-active" "winWS")
+    is_valid_vm=("arch" "winVMdesign" "winVMmain" "winVMwork")
 
     for vm_in_list in ${is_valid_vm[@]}; do
         if [[ ${vm_in_list} = ${vm} ]]; then
